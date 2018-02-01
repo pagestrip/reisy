2d8d8a833cd431359a8e8f23c93c5c324f88a13f
Immanuel Bauer <gimmix.at@gmail.com>
2018-02-01 15:53:46 +0100

    added helper script to generate prefixes static config

---
+++ scripts/generatestatic.js
@@ -0,0 +1,20 @@
+const generateData = require('inline-style-prefixer/generator');
+
+const browserList = {
+  chrome: 42,
+  android: 4,
+  firefox: 40,
+  ios_saf: 6,
+  safari: 6,
+  ie: 10,
+  ie_mob: 11,
+  edge: 12,
+  opera: 16,
+  op_mini: 12,
+  and_uc: 9,
+  and_chr: 46
+}
+
+generateData(browserList, {
+  staticPath: `${__dirname}/staticData.js`
+})
